c91421bc1a5eaff39e8cd9f18868b1390d98b117
Wrichmond Well A. Chua <59139222+WrichmondChua@users.noreply.github.com>
2023-04-12 09:44:25 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var container = document.querySelector('.container');
+var colorCard = document.querySelector('.color-card');
+var btn = document.querySelector("button");
+var hex = document.querySelector("span");
+function getNewColor() {
+    var symbols = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color = color + symbols[Math.floor(Math.random() * 16)];
+    }
+    colorCard.style.backgroundColor = color;
+}
+btn.addEventListener("click", getNewColor);
